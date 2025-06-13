b121707be04774527360b4457f0d2e9692a60b4f
Swaraj Phalke <swarajphalke555@gmail.com>
2025-06-13 21:04:56 +0530

    Abstraction

---
+++ Abstraction/Abstraction/Abstraction1.cs
@@ -0,0 +1,45 @@
+﻿namespace Abstraction
+{
+    public class Abstraction1
+    {
+        static void Main(String[] args)
+        {
+            Vehicle c = new Car();
+            c.start();
+            c.sound();
+
+            Vehicle b = new Bike();
+            b.start();
+            b.sound();
+
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public void start()
+        {
+            Console.WriteLine("Vehicle started...");
+        }
+
+        public abstract void sound();
+    }
+
+    class Car : Vehicle
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Car making sound");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Bike Making sound");
+         }
+
+    }
+
+}
---
+++ Abstraction/Abstraction/Abstraction2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class Abstraction2
+    {
+        static void Main(String[] args)
+        {
+            Payment p1 = new CreditCardPayment();
+            p1.ConnectToBank();
+            p1.ValidateDetails();
+            p1.MakePayment();
+
+            Console.WriteLine();
+
+            Payment p2 = new UPIPayment();
+            p2.ConnectToBank();
+            p2.ValidateDetails();
+            p2.MakePayment();
+        }
+    }
+
+    abstract class Payment
+    {
+        public void ConnectToBank()
+        {
+            Console.WriteLine("Connecting to bank...");
+        }
+
+        public abstract void ValidateDetails();
+        public abstract void MakePayment();
+    }
+
+    class CreditCardPayment:Payment
+    {
+        public override void ValidateDetails()
+        {
+            Console.WriteLine("Validating Credit Card number and CVV...");
+        }
+        public override void MakePayment()
+        {
+            Console.WriteLine("Processing Credit Card Payment...");
+        }
+    }
+
+    class UPIPayment:Payment
+    {
+        public override void ValidateDetails()
+        {
+            Console.WriteLine("Validating UPI and PIN...");
+        }
+
+        public override void MakePayment()
+        {
+            Console.WriteLine("Processing UPI Payment");
+        }
+
+    }
+}
