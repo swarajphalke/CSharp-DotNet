f3a927683230f32bd130486ccf45f8cd9b1bcf8c
Swaraj Phalke <swarajphalke555@gmail.com>
2025-06-13 21:04:19 +0530

    Polymorphism

---
+++ Polymorphism/Polymorphism/Overloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Overloading
+    {
+        static void Main(String[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(c.Add(4,5));
+            Console.WriteLine(c.Add(2.5f,2.5f));
+            Console.WriteLine(c.Add("Swa","raj"));
+
+        }
+    }
+
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public float Add(float a , float b)
+        {
+            return a + b;
+        }
+
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Overriding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Overriding
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal();
+            Animal a2 = new Dog();
+            Animal a3 = new Cat();
+
+            a1.Speak();
+            a2.Speak();
+            a3.Speak();
+        }
+    }
+
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+    
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The Dog barks");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The Cat meows");        }
+    }
+}
