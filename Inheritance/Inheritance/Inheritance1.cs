fea777e60f765a999064d5d0cf4cb00b33398acb
Swaraj Phalke <swarajphalke555@gmail.com>
2025-06-13 21:03:30 +0530

    Inheritance

---
+++ Inheritance/Inheritance/Inheritance1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Inheritance1
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("\n🔷 Single Inheritance : ");
+            Dog d = new Dog();
+            d.Eat();    // From Animal
+            d.Bark();   // From Dog
+
+            Console.WriteLine();
+
+            Console.WriteLine("\n🔷 Multilevel Inheritance:");
+            Puppy p = new Puppy();
+            p.Eat();    // From Animal
+            p.Bark();   // From Dog
+            p.Weep();   // From Puppy
+
+            Console.WriteLine();
+
+            Console.WriteLine("\n🔷 Hierarchical Inheritance:");
+            Cat c = new Cat();
+            c.Eat();    // From Animal
+            c.Meow();   // From Cat
+        }
+    }
+
+    //Single Inheritance
+    internal class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal can Eat...");
+        }
+    }
+
+    internal class Dog:Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog can Bark...");
+        }
+    }
+
+    // Multilevel Inheritance
+    internal class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Puppy Weeps");
+        }
+    }
+
+    // Hierarchical Inheritance
+    internal class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Cat Meows");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Inheritance2.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace Inheritance
+{
+    internal class Inheritance2
+    {
+        static void Main(String[] args)
+        {
+            Car c = new Car("Toyota", 190);
+            c.DisplayFuelType();    // Call Child class method
+            c.DisplayInfo();    //Call base class method
+        }
+    }
+
+    internal class Vehicle
+    {
+        string Brand;
+        int Speed;
+
+        public Vehicle(string Brand, int speed)
+        {
+            this.Brand = Brand;
+            this.Speed = speed;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Vehicle Brand : " + Brand + " And Speed is : " + Speed);
+        }
+    }
+
+    internal class Car : Vehicle
+    {
+        public string FuelType = "Disel";
+
+        public Car(string Brand, int Speed) : base(Brand, Speed)
+        {
+
+        }
+        public void DisplayFuelType()
+        {
+            Console.WriteLine(FuelType);
+        }
+    }
+}
