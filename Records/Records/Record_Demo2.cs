79008b56f4a8c4baf1e3fada70429ba7e3e74092
Swaraj Phalke <swarajphalke555@gmail.com>
2025-06-30 21:05:17 +0530

    Record 2

---
+++ Records/Records/Record_Demo2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record
+{
+    public record Book(string Title, string Author, double Price);
+    internal class Record_Demo2
+    {
+        static void Main()
+        {
+            var v1 = new Book("The Alchemist", "Swaraj Phalke", 340.55);
+            var v2 = new Book("The Alchemist", "Swaraj Phalke", 340.55);
+            var v3 = new Book("Wings of Fire", "A.P.J. Abdul Kalam", 400.34);
+
+            Console.WriteLine(v1);
+
+            Console.WriteLine(v1==v2);
+            Console.WriteLine(v1==v3);
+        }
+    }
+}
