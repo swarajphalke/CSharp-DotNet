cea626a326c8bc69e5cd6558808d3288c6374268
Swaraj Phalke <swarajphalke555@gmail.com>
2025-06-30 21:05:30 +0530

    Tuple

---
+++ Records/Records/Tuple_Demo.cs
@@ -0,0 +1,25 @@
+﻿using Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record
+{
+    internal class Tuple_Demo
+    {
+        static void Main()
+        {
+            var p1 = Tuple.Create(34, "ankit", "kulkarni");
+            Console.WriteLine(p1);
+        }
+
+        public static void displaytuple(Tuple<int, string, string> person)
+        {
+            Console.WriteLine(person.Item1);
+            Console.WriteLine(person.Item2);
+            Console.WriteLine(person.Item3);
+        }
+    }
+}
