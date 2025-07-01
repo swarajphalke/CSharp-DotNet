b0016aa6ffa0dbd9759a1688a84f86f79e57572e
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-01 20:22:30 +0530

    Stack

---
+++ Collection_Non-Generic/Collection_Non-Generic/Stack_Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class Stack_Demo
+    {
+        static void Main()
+        {
+            Stack s = new Stack();
+
+            s.Push("Swaraj");
+            s.Push("Siddharth");
+            s.Push("Aditya");
+            s.Push("Mayuresh");
+            s.Push(560.45);
+
+            Console.WriteLine("top item: "+ s.Peek());  //just check the top item
+            Console.WriteLine("popped item: "+s.Pop()); //last item removed
+
+            foreach(object o in s)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
