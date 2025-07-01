2179c89817a8fe309a2ad7ce8812a46fa320e805
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-01 20:22:46 +0530

    Queue

---
+++ Collection_Non-Generic/Collection_Non-Generic/Queue_Demo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class Queue_Demo
+    {
+        static void Main()
+        {
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue(3443.03);
+            q.Enqueue("Phalke");
+            q.Enqueue(342.34);
+            q.Enqueue(true);
+
+            Console.WriteLine("top item: "+q.Peek());
+            Console.WriteLine("dequeued item: " + q.Dequeue());
+
+            foreach(object o in q)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
