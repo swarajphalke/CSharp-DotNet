9da1f4700b6135aee24d899ef094a84f4abbef55
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-01 20:22:57 +0530

    HashTable

---
+++ Collection_Non-Generic/Collection_Non-Generic/Hashtable_Demo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class Hashtable_Demo
+    {
+        static void Main()
+        {
+            Hashtable h = new Hashtable();
+            h.Add(34, "sss");
+            h.Add("www", 78);
+            h.Add("456.45", "aaa");
+            h.Add(true, 678);
+            h.Add('c', 20);
+
+            Console.WriteLine("----------Elements in Hashtable----------");
+            foreach(DictionaryEntry o in h)
+            {
+                Console.WriteLine(o.Key+ "   "+o.Value);
+            }
+
+            Console.WriteLine("\n----------Keys in Hashtable----------");
+            foreach (object o in h.Keys)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("\n----------Values in Hashtable----------");
+            foreach(object o in h.Values)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("\n----------After Removing Elements----------");
+            h.Remove("www");
+            foreach(object o in h.Values)
+            {
+                Console.WriteLine(o);
+            }
+
+        }
+
+    }
+}
---
+++ Collection_Non-Generic/Collection_Non-Generic/SortedList_Demo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class SortedList_Demo
+    {
+        public static void Main()
+        {
+            SortedList s = new SortedList();
+            s.Add(34, "sss");
+            s.Add(12, "www");
+            s.Add(72, "aaa");
+            s.Add(05, "rrr");
+            //s.Add(12, "aaa");   duplicates are not allowed in sorted list
+
+            //s.Remove(34); //first element
+            //s.RemoveAt(1); //second element
+
+            Console.WriteLine("----------All Key Value Pairs----------");
+            foreach(DictionaryEntry o in s)
+            {
+                Console.WriteLine(o.Key+ "   "+o.Value);
+            }
+
+            Console.WriteLine("\n----------Keys Only----------");
+            foreach(object o in s.Keys)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("\n----------Values Only----------");
+            foreach (object o in s.Values)
+            {
+                Console.WriteLine(o);
+            }
+
+
+
+        }
+    }
+}
