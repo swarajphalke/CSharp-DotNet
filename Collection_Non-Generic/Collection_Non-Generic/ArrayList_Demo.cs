7293d610aa1f461e8a6073fedbd2b6eaa912ace0
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-01 20:22:19 +0530

    ArrayList

---
+++ Collection_Non-Generic/Collection_Non-Generic/ArrayList_Demo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class ArrayList_Demo
+    {
+        static void Main()
+        {
+            ArrayList arr = new ArrayList();
+
+            arr.Add(12);
+            arr.Add("Swaraj");
+            arr.Add(789.90);
+            arr.Add(true);
+            arr.Add('c');
+
+            //Console.WriteLine(arr[0]);
+            //Console.WriteLine(arr[1]);
+            //Console.WriteLine(arr[2]);
+            //Console.WriteLine(arr[3]);
+            //Console.WriteLine(arr[4]);
+
+            string[] s = new string[5] { "aaa", "bbb","ccc","ddd","eee" };
+            arr.AddRange(s);
+
+            arr.Insert(1, "Viraj");
+
+            //arr.Remove(true);
+            //arr.RemoveAt(0);
+            //arr.RemoveRange(5, 1);
+
+            Console.WriteLine("\nTotal elements in ArrayList: {0}", arr.Count);
+
+            Console.WriteLine("\nFinal elements in ArrayList:");
+            foreach (object o in arr)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("\nCurrent capacity of ArrayList: {0}", arr.Capacity);
+
+        }
+    }
+}
---
+++ Collection_Non-Generic/Collection_Non-Generic/ArrayList_Demo1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class ArrayList_Demo1
+    {
+     static void Main()
+        {
+            ArrayList arr = new ArrayList();
+
+            arr.Add(100);
+            arr.Add(200);
+
+            Student s1 = new Student();
+            s1.Rno = 1;
+            s1.Name = "Swaraj";
+            s1.Address = "Pune";
+            s1.Marks = 67;
+            arr.Add(s1);
+
+            IEnumerator a = arr.GetEnumerator();
+            while (a.MoveNext())
+            {
+                if(a.Current is Student)
+                {
+                    Student s5 = (Student)a.Current;
+                    Console.WriteLine(s5.Rno+" "+s5.Name+" "+s5.Address+" "+s5.Marks);
+                }
+                else if(a.Current is int)
+                {
+                    Console.WriteLine(a.Current);
+                }
+            }
+        }   
+    }
+
+    class Student
+    {
+        public int Rno { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Marks { get; set; }
+    }
+}
