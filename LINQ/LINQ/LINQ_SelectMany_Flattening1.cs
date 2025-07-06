aa9786cd1fd8fe6ec2a3c19356f9361dba9b7fab
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-06 20:20:58 +0530

    updated

---
+++ LINQ/LINQ/LINQ_SelectMany_Flattening1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class LINQ_SelectMany_Flattening1
+    {
+        static void Main()
+        {
+            List<string[]> teams = new List<string[]>
+            {
+                new string[] { "Swaraj","Viraj"},
+                new string[] {"Mayuresh","Aditya"}
+            };
+
+            var result = teams.Select(team => team);
+
+            Console.WriteLine("Using Select: ");
+            foreach (var team in result)
+            {
+                Console.WriteLine("Team: ");
+                foreach (var member in team)
+                {
+                    Console.WriteLine("  " + member);
+                }
+            }
+        }
+    }
+}
---
+++ LINQ/LINQ/LINQ_SelectMany_Flattening2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class LINQ_SelectMany_Flattening2
+    {
+        static void Main()
+        {
+            List<string[]> teams = new List<string[]>
+            {
+                new string[] {"Swaraj","Viraj"},
+                new string[] {"Mayuresh","Aditya"}
+            };
+
+            var result = teams.SelectMany(team => team);
+
+            Console.WriteLine("Using SelectMany:");
+            foreach (var member in result)
+            {
+                Console.WriteLine(member);
+            }
+        }
+    }
+}
