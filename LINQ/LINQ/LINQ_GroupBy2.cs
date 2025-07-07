2947e5f516e3cd0aad67efa0e964d2f5d0186810
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-07 23:36:41 +0530

    LINQ Intermediate

---
+++ LINQ/LINQ/LINQ_GroupBy2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class LINQ_GroupBy2
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var groups = numbers.GroupBy(n => n % 2 == 0 ? "Even" : "Odd");
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key + "Numbers: ");
+                foreach (var item in group)
+                {
+                    Console.WriteLine("  " + item);
+                }
+            }
+        }
+    }
+}
---
+++ LINQ/LINQ/LINQ_Join_GroupJoin1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class LINQ_Join_GroupJoin1
+    {
+
+        static void Main() {
+            List<Student> students = new List<Student>
+        {
+            new Student {Name = "Swaraj",DepartmentId=1},
+            new Student {Name = "Om",DepartmentId=2},
+            new Student {Name = "Purvesh",DepartmentId=1}
+        };
+
+            List<Department> departments = new List<Department>
+        {
+            new Department {Id = 1, DeptName="Computer" },
+            new Department {Id = 2, DeptName = "Mechanical"}
+        };
+
+            var result = students.Join(
+                departments,
+                student => student.DepartmentId,
+                dept => dept.Id,
+                (student, dept) => new { student.Name, dept.DeptName}
+
+                );
+
+            Console.WriteLine("Inner Join Result: ");
+            foreach(var item in result)
+            {
+                Console.WriteLine($"{item.Name} - {item.DeptName}");
+            }
+    }
+    }
+    class Student
+    {
+        public string Name;
+        public int DepartmentId;
+    }
+
+    class Department
+    {
+        public int Id;
+        public string DeptName;
+    }
+}
+ 
