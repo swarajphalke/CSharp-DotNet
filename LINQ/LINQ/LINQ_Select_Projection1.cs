c7aa993d5c8f2bb1667f328a3df2257aee1c6587
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-09 23:17:48 +0530

    updated

--- LINQ/LINQ/LINQ_Select_Projection1.cs
+++ LINQ/LINQ/LINQ_Select_Projection1.cs
@@ -10,7 +10,6 @@ namespace LINQ
     {
         static void Main()
         {
-            
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             var doubled = numbers.Select(n => n * 2);
