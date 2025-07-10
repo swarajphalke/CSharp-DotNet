165d9510f93765186d1110fca2c149a8ce74aed2
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-10 23:13:36 +0530

    updated

--- LINQ/LINQ/LINQ_SelectMany_Flattening1.cs
+++ LINQ/LINQ/LINQ_SelectMany_Flattening1.cs
@@ -12,7 +12,6 @@ namespace LINQ
         {
             List<string[]> teams = new List<string[]>
             {
-                
                 new string[] { "Swaraj","Viraj"},
                 new string[] {"Mayuresh","Aditya"}
             };
