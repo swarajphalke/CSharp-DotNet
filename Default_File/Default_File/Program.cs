eadf9cc1b86d098c3a77e45882a4c554d15bb87a
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-14 19:19:00 +0530

    Default File

---
+++ Default_File/Default_File/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// added custom default file (applied only on static files)
+DefaultFilesOptions d = new DefaultFilesOptions();
+d.DefaultFileNames.Clear();
+d.DefaultFileNames.Add("index.html");
+app.UseDefaultFiles(d);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
