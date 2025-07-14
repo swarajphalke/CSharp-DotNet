09f67667791a115d34487cf769e260ddda6901d5
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-14 19:18:34 +0530

    Custom Middleware

---
+++ Custom_Middleware/Custom_Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,9 @@
+﻿public class MyCustomMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        await context.Response.WriteAsync("Inside MyMiddleware (Before next)\n");
+        await next(context);
+        await context.Response.WriteAsync("Inside MyMiddleware (After next)\n");
+    }
+}
