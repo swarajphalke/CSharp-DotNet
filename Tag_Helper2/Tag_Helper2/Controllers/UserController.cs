89c8b4ed85d79c4420ab6fc516d2360198c840ae
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-15 20:50:57 +0530

    advanced Tag Helper

---
+++ Tag_Helper2/Tag_Helper2/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tag_Helper2.Models;
+
+namespace Tag_Helper2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(); // Loads the Register.cshtml view
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // If valid, redirect to Success page
+                return RedirectToAction("Success");
+            }
+
+            // Otherwise show form again with validation errors
+            return View(user);
+        }
+
+        public IActionResult Success()
+        {
+            return Content("Registration Successful!");
+        }
+    }
+}
