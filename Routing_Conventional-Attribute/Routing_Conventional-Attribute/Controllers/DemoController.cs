06fa3aeb214dd06d05e304aba7fd66250309bf6c
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-15 20:52:29 +0530

    Routing Conventional Attribute

---
+++ Routing_Conventional-Attribute/Routing_Conventional-Attribute/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing_Conventional_Attribute.Controllers
+{
+    [Route("Practice")]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("demo")] // → /Practice/demo
+        public IActionResult Demo()
+        {
+            ViewData["message"] = "Welcome to ASP.NET Core!";
+            ViewData["Year"] = 2025;
+            return View();
+        }
+        [Route("numbers/{id}")] // → /Practice/numbers/22
+        public IActionResult numbers(int id)
+        {
+            ViewData["id1"] = 1;
+            ViewData["id2"] = 2025;
+            ViewData["fromUrl"] = id;
+            return View();
+        }
+
+    }
+}
