615dba163a8c2498760921c88c9ed2bba71b54ed
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-16 13:17:27 +0530

    Custom Tag Helper

---
+++ Custom_TagHelper/Custom_TagHelper/Mycusttag.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace Custom_TagHelper
+{
+    [HtmlTargetElement("Demo")]
+    public class MyCustTag : TagHelper
+    {
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "CustomTagHelper";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<span>Hi ! {0}</span>", this.Name);
+
+            output.Content.SetHtmlContent(sb.ToString());
+        }
+    }
+}
