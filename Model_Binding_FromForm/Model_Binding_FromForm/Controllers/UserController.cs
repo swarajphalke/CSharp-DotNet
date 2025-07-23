b3a2b1c391171527dc3dcb64500fbbbc657955c9
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-23 14:07:14 +0530

    From Form Model Binding

---
+++ Model_Binding_FromForm/Model_Binding_FromForm/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model_Binding_FromForm.Models;
+namespace Model_Binding_FromForm.Controllers
+{
+    public class UserController : Controller
+    {
+        // inside controller
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Users user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            return RedirectToAction("successful");
+        }
+
+        public string successful()
+        {
+            return "user created sucessfully";
+        }
+    }
+}
---
+++ Model_Binding_FromForm/Model_Binding_FromForm/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model_Binding_FromForm.Models
+{
+    // The FromForm attribute instructs the model binder to fetch the data from the form body of the request and try to bind
+    // it to the specified action method parameters or model properties.
+    // where you can use from form
+    // traditional html form
+    // file uploads
+    // complex modelbinding with form data
+    // inside model folder create a class
+    public class Users
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Mobile { get; set; }
+    }
+}
