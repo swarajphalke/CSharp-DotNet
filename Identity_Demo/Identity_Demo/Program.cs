df040c1d8a33f7d2656cf1977b9aa117ef0efa21
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-28 23:20:06 +0530

    Identity Updated

--- Identity_Demo/Identity_Demo/Program.cs
+++ Identity_Demo/Identity_Demo/Program.cs
@@ -21,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     
+
     
 }
 
