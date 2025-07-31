6c59a31b074af27c0e754660836c4f70208f9485
Swaraj Phalke <swarajphalke555@gmail.com>
2025-07-31 21:09:24 +0530

    Query String Updated

--- Query_String/Query_String/Controllers/StudentController.cs
+++ Query_String/Query_String/Controllers/StudentController.cs
@@ -7,7 +7,6 @@ namespace Query_String.Controllers
 
         public IActionResult Details(int id, string name)
         {
-            
             // GET: /Student/Details?id=1&name=Aditya
             ViewBag.StudentId = id;
             ViewBag.StudentName = name;
