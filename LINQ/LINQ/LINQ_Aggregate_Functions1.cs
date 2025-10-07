f22a2c48588c72cf504323db82dbed452963694d
Swaraj Phalke <swarajphalke555@gmail.com>
2025-10-07 22:55:01 +0530

    updated

--- LINQ/LINQ/LINQ_Aggregate_Functions1.cs
+++ LINQ/LINQ/LINQ_Aggregate_Functions1.cs
@@ -15,6 +15,7 @@ namespace LINQ
             int total = marks.Sum();
             double average = marks.Average();
             int count = marks.Count();
+            
             int min = marks.Min();
             int max = marks.Max();
 
